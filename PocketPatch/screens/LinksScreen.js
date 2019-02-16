289cc0df849128cba1cb95d8b1e4a0a3d62871af
anna-deng <annadeng2020@u.northwestern.edu>
2019-02-16 16:37:59 -0600

    moving progress bar

--- PocketPatch/screens/LinksScreen.js
+++ PocketPatch/screens/LinksScreen.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import * as Progress from 'react-native-progress';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
     title: 'Links',
   };
 
+  constructor(props) {
+   super(props);
+   this.state = { progress: 0 };
+
+
+  setInterval(() => (
+    this.setState(previousState => (
+      { progress: this.state.progress + .1 }
+    ))
+  ), 1000);
+}
+
   render() {
     return (
       <ScrollView style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+          <Text>Hello</Text>
+
+          <Progress.Bar progress={this.state.progress} width={200} />
       </ScrollView>
     );
   }
