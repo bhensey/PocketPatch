b7befc60912ffa525028aa4fdfbe9cbb1c43ecfe
anna-deng <annadeng2020@u.northwestern.edu>
2019-02-17 17:28:18 -0600

    added emoji icons

--- PocketPatch/screens/LinksScreen.js
+++ PocketPatch/screens/LinksScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { View, ScrollView, StyleSheet, Text} from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import * as Progress from 'react-native-progress';
+import {Icon} from 'react-native-elements';
+
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -22,14 +24,26 @@ export default class LinksScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
+      <View style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
-          {/*<Image style={styles.sadFace} source={require('./sadface.png')}/>*/}
-          <Progress.Bar style={styles.progressBar} progress={this.state.progress} color='#5480AF' width={350} margin={0, 10} />
-          {/*<Image style={styles.happyFace} source={require('./happyface.png')} />*/}
-          <Text style={styles.omyMessage}> HELP ME BREATHE, OMY. </Text>
-      </ScrollView>
+           <View style={styles.progressBarContainer}>
+              <Icon
+               style={styles.sadFace}
+               name='frowno'
+               type='antdesign'
+               color="#c06137"
+               />
+              <Progress.Bar style={styles.progressBar} progress={this.state.progress} color='#5480AF' width={300} margin={5} />
+              <Icon
+              style={styles.happyFace}
+              name='smileo'
+              type='antdesign'
+              color="#7f4dda"
+              />
+          </View>
+        <Text style={styles.omyMessage}> HELP ME BREATHE, OMY. </Text>
+      </View>
     );
   }
 }
@@ -38,25 +52,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#C8E0F4',
+    backgroundColor: '#fff3cf',
   },
 
   progressBar: {
     borderWidth: 5,
   },
 
+  progressBarContainer:{
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+
   omyMessage: {
-    marginTop: 450,
-    fontSize: 30,
+    marginTop: 425,
+    fontSize: 28,
     fontFamily: 'Cochin',
     color: '#5480AF'
   },
 
   sadFace: {
-    marginLeft: 0,
+    marginLeft: 1,
   },
 
   happyFace: {
-    marginRight: 0,
+    marginRight: 1,
   }
 });
