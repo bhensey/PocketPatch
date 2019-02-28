331ab8347fbfe876a3f878d914e1a0d07429c18f
Bennett Hensey <bennett.hensey@gmail.com>
2019-02-28 16:55:39 -0600

    Adjusted size of locus

--- PocketPatch/screens/SettingsScreen.js
+++ PocketPatch/screens/SettingsScreen.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native'
-import { Button, Card } from 'react-native-elements';
-import Confetti from 'react-native-confetti';
-import { ExpoConfigView } from '@expo/samples';
-
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+import { Button, Card } from "react-native-elements";
+import Confetti from "react-native-confetti";
+import { ExpoConfigView } from "@expo/samples";
 
 export default class SettingsScreen extends React.Component {
-
   componentDidMount() {
     if (this._confettiView) {
       this._confettiView.startConfetti();
@@ -20,39 +18,43 @@ export default class SettingsScreen extends React.Component {
           title="Back to Home"
           type="clear"
           onPress={() => navigation.popToTop()}
-
         />
-
       )
-    }
+    };
   };
 
   render() {
     return (
-      <View style={[styles.container, {backgroundColor: "#ce95f4"}]}>
-        <Confetti ref={(node) => this._confettiView = node} duration={4000} timeout={10} confettiCount={500} />
+      <View style={[styles.container, { backgroundColor: "#ce95f4" }]}>
+        <Confetti
+          ref={node => (this._confettiView = node)}
+          duration={4000}
+          timeout={10}
+          confettiCount={500}
+        />
         <Card containerStyle={styles.card} title="Way to go!">
           <View style={styles.cardContent}>
-
-            <Image style={{ width: 300, height: 300 }} source={require('../assets/images/start-end-images/Lotus-02.png')} />
+            <Image
+              style={{ width: 300, height: 300, resizeMode: "contain" }}
+              source={require("../assets/images/start-end-images/Lotus-02.png")}
+            />
             <Text>You've earned 10 breathing points!</Text>
           </View>
         </Card>
       </View>
-    )
+    );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   cardContent: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   card: {
     borderRadius: 10
