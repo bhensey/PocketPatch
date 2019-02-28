0a93d456def8799b498add93f6e793e1291cf2ef
Bennett Hensey <bennett.hensey@gmail.com>
2019-02-28 16:43:47 -0600

    Repositioned elements on homepage

--- PocketPatch/screens/HomeScreen.js
+++ PocketPatch/screens/HomeScreen.js
@@ -13,44 +13,67 @@ import {
   TouchableHighlight
 } from "react-native";
 import { WebBrowser } from "expo";
-import {Button} from 'react-native-elements';
+import { Button } from "react-native-elements";
 
 import { MonoText } from "../components/StyledText";
-
 
 export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
-
   }
 
   static navigationOptions = {
     header: null
   };
 
-
   render() {
-    console.log("render")
+    console.log("render");
 
     return (
-      <View style={{flex: 1, backgroundColor: "#DDDDFF"}}>
+      <View style={{ flex: 1, backgroundColor: "#DDDDFF", paddingBottom: 50 }}>
         <View style={styles.container}>
-        <Image style={{height: 200, resizeMode: 'contain'}} source={require('../assets/images/start-end-images/logo-01.png')}/>
-        <TouchableOpacity style={styles.container} onPress={() => this.props.navigation.navigate('Links')}>
-          <View>
-            <Image style={{width: 250, height: 200, resizeMode: 'contain'}} source={require('../assets/images/start-end-images/OK-01-01.png')} />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.container} onPress={() => this.props.navigation.navigate('Links')}>
-          <View>
-            <Image style={{width: 250, height: 200, resizeMode: 'contain'}} source={require('../assets/images/start-end-images/upset-02-02.png')} />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.container} onPress={() => this.props.navigation.navigate('Links')}>
-          <View>
-            <Image style={{width: 250, height: 200, resizeMode: 'contain'}} source={require('../assets/images/start-end-images/wound-up-03-03.png')} />
-          </View>
-        </TouchableOpacity>
+          <Image
+            style={{
+              height: 200,
+              resizeMode: "contain",
+              marginTop: 100,
+              marginBottom: 50
+            }}
+            source={require("../assets/images/start-end-images/logo-01.png")}
+          />
+          <TouchableOpacity
+            style={styles.container}
+            onPress={() => this.props.navigation.navigate("Links")}
+          >
+            <View>
+              <Image
+                style={{ height: 100, resizeMode: "contain" }}
+                source={require("../assets/images/start-end-images/OK-01-01.png")}
+              />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.container}
+            onPress={() => this.props.navigation.navigate("Links")}
+          >
+            <View>
+              <Image
+                style={{ height: 100, resizeMode: "contain" }}
+                source={require("../assets/images/start-end-images/upset-02-02.png")}
+              />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.container}
+            onPress={() => this.props.navigation.navigate("Links")}
+          >
+            <View>
+              <Image
+                style={{ height: 100, resizeMode: "contain" }}
+                source={require("../assets/images/start-end-images/wound-up-03-03.png")}
+              />
+            </View>
+          </TouchableOpacity>
           {/*<Button
             buttonStyle= {{width: 150, backgroundColor: 'red'}}
             title="Angry"
@@ -80,5 +103,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#DDDDFF",
     flex: 1
-  },
+  }
 });
